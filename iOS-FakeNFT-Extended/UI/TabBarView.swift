811c121255e62c90e43446aab8d473877801addf
Danil Kazakov <dani4upa586@gmail.com>
2025-09-12 13:12:24 +0900

    add appearance to TabBar

--- iOS-FakeNFT-Extended/UI/TabBarView.swift
+++ iOS-FakeNFT-Extended/UI/TabBarView.swift
@@ -1,16 +1,28 @@
 import SwiftUI
 
 struct TabBarView: View {
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.systemBackground
+        
+        // Настройка цветов для неактивных
+        appearance.stackedLayoutAppearance.normal.iconColor = UIColor.segmentActive
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.segmentActive]
+        
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some View {
         TabView {
-            TestCatalogView()
+            CatalogView()
                 .tabItem {
                     Label(
                         NSLocalizedString("Tab.catalog", comment: ""),
-                        systemImage: "square.stack.3d.up.fill"
+                        systemImage: .squareStack,
                     )
                 }
-                .backgroundStyle(.background)
         }
     }
 }
