df6a9654e25ff1be2ddde6d2e3f1cc736757e4f0
Danil Kazakov <dani4upa586@gmail.com>
2025-09-15 17:04:05 +0900

    expand Nft model

--- iOS-FakeNFT-Extended/Models/Network/Nft.swift
+++ iOS-FakeNFT-Extended/Models/Network/Nft.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-struct Nft: Decodable {
+struct Nft: Decodable, Identifiable {
+    let name: String
+    let rating: Int
+    let price: Double
     let id: String
     let images: [URL]
 }
