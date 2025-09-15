85e93fbe285dc3b9d87e8d5581e5eeb8b2ca18f9
Danil Kazakov <dani4upa586@gmail.com>
2025-09-15 17:03:45 +0900

    add Nft+Mock

---
+++ iOS-FakeNFT-Extended/UI/Catalog/NftCollection/Mocks/Nft+Mock.swift
@@ -0,0 +1,22 @@
+//
+//  Nft+Mock.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Danil Kazakov on 15.09.2025.
+//
+
+import Foundation
+
+extension Nft {
+    static let mockNft = mockNfts[0]
+    
+    static let mockNfts = (1...20).map {nft in
+        Nft(
+            name: "Angel Alvarado" ,
+            rating: 4,
+            price: 49.77,
+            id: "b3907b86-37c4-4e15-95bc-7f8147a9a660" + "\(nft)",
+            images: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/White/Lumpy/1.png")!]
+        )
+    }
+}
