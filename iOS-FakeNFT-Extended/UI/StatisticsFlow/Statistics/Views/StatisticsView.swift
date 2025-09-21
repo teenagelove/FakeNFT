24323d64b360eea31a7234312b43163b6746e37a
Svetlik-B <svetlik.igorevna@gmail.com>
2025-09-21 20:59:45 +0400

    [Refactor] вынести константы и ячейку списка

--- iOS-FakeNFT-Extended/UI/StatisticsFlow/Statistics/Views/StatisticsView.swift
+++ iOS-FakeNFT-Extended/UI/StatisticsFlow/Statistics/Views/StatisticsView.swift
@@ -1,63 +1,81 @@
 import SwiftUI
 
+private enum Constant {
+    static let avatarSize: CGFloat = 28
+    static let grayUniversal: Color = .init(UIColor(hexString: "#625C5C"))
+    static let margin: CGFloat = 16
+}
+
 struct StatisticsView: View {
     @Bindable var viewModel: StatisticsViewModel
     var body: some View {
-        List(viewModel.sortedUsers) { user in
-            HStack {
-                Text(user.rating).font(.caption1)
-                HStack {
-                    AsyncImage(url: user.avatar) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                    } placeholder: {
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .foregroundStyle(Color(UIColor(hexString: "#625C5C")))
+        List(viewModel.sortedUsers) { UserView(user: $0) }
+            .listStyle(.plain)
+            .task { await viewModel.load() }
+            .overlay {
+                if viewModel.state == .loading {
+                    ProgressView()
+                }
+            }
+            .alert(
+                "Не удалось получить данные",
+                isPresented: $viewModel.hasError
+            ) {
+                Button("Отмена") { viewModel.state = .loaded }
+                Button("Повторить") { Task { await viewModel.load() } }
+            }
+            .confirmationDialog(
+                "Сортировка",
+                isPresented: $viewModel.showSortMenu,
+                titleVisibility: .visible
+            ) {
+                ForEach(StatisticsViewModel.SortOrder.allCases, id: \.self) { caseName in
+                    Button(caseName.rawValue) {
+                        viewModel.sortOrder = caseName
                     }
-                    .frame(width: 28, height: 28)
-                    .clipShape(Circle())
-                    Text(user.name)
-                    Spacer()
-                    Text(user.nfts.count.formatted())
                 }
-                .font(.headline3)
-                .padding()
-                .frame(height: 80)
-                .background(Color(uiColor: .segmentInactive))
-                .clipShape(RoundedRectangle(cornerRadius: 12))
+                Button("Закрыть", role: .cancel) {}
             }
-            .listRowSeparator(.hidden)
-            .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 8, trailing: 16))
-        }
-        .listStyle(.plain)
-        .task { await viewModel.load() }
-        .overlay {
-            if viewModel.state == .loading {
-                ProgressView()
-            }
-        }
-        .alert(
-            "Не удалось получить данные",
-            isPresented: $viewModel.hasError
-        ) {
-            Button("Отмена") { viewModel.state = .loaded }
-            Button("Повторить") { Task { await viewModel.load() } }
-        }
-        .confirmationDialog(
-            "Сортировка",
-            isPresented: $viewModel.showSortMenu,
-            titleVisibility: .visible
-        ) {
-            ForEach(StatisticsViewModel.SortOrder.allCases, id: \.self) { caseName in
-                Button(caseName.rawValue) {
-                    viewModel.sortOrder = caseName
+    }
+}
+
+private struct UserView: View {
+    var user: UserViewModel
+    var body: some View {
+        HStack {
+            Text(user.rating).font(.caption1)
+            HStack {
+                AsyncImage(url: user.avatar) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .foregroundStyle(Constant.grayUniversal)
                 }
+                .frame(width: Constant.avatarSize, height: Constant.avatarSize)
+                .clipShape(Circle())
+                Text(user.name)
+                Spacer()
+                Text(user.nfts.count.formatted())
             }
-            Button("Закрыть", role: .cancel) {}
+            .font(.headline3)
+            .padding()
+            .frame(height: 80)
+            .background(Color(uiColor: .segmentInactive))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
         }
+        .listRowSeparator(.hidden)
+        .listRowInsets(
+            EdgeInsets(
+                top: 0,
+                leading: Constant.margin,
+                bottom: 8,
+                trailing: Constant.margin
+            )
+        )
     }
 }
 
