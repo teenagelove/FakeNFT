d00572f25dbc3c461348e04ea7a1454bdfc9f83c
Aleksandr <Al3ksandr213@yandex.ru>
2025-10-06 18:08:31 +0300

    refactor: general refactoring

--- iOS-FakeNFT-Extended/DesignSystem/Images.swift
+++ iOS-FakeNFT-Extended/DesignSystem/Images.swift
@@ -8,5 +8,9 @@
 extension String {
     static let squareStack = "square.stack.3d.up.fill"
     static let star = "star.fill"
+    static let heart = "heart.fill"
     static let chevronLeft = "chevron.left"
+    static let triangle = "exclamationmark.triangle.fill"
+    static let clockwise = "arrow.clockwise"
+    static let crossedFlags = "flag.2.crossed.fill"
 }
--- iOS-FakeNFT-Extended/Services/ServicesAssembly.swift
+++ iOS-FakeNFT-Extended/Services/ServicesAssembly.swift
@@ -7,6 +7,7 @@ final class ServicesAssembly {
     private let networkClient: NetworkClient
     private let nftStorage: NftStorage
 
+    
     init(
         networkClient: NetworkClient,
         nftStorage: NftStorage
